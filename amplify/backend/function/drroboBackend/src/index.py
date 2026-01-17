456322008b98fc8e961dc8edddbd73e7656e7f53
jayanibhatwadiya <jayani.bhatwadiya@oceanprime.co>
2026-01-17 13:25:47 +0530

    “Update”

--- amplify/backend/function/drroboBackend/src/index.py
+++ amplify/backend/function/drroboBackend/src/index.py
@@ -1,19 +1,4 @@
-import sys
-import os
 from mangum import Mangum
 from main import app
 
-handler = Mangum(app, lifespan="off")
-
-
-# Adds the current directory to the path so Lambda can find 'main.py' and your 'src' folder
-current_dir = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(current_dir)
-
-from main import handler as fastapi_handler
-
-def handler(event, context):
-    return fastapi_handler(event, context)
-
-# 4. Lambda Handler
 handler = Mangum(app, lifespan="off")
